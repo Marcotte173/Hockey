95fc74f10bb1d6c8a07a5634c4dde0493bc81ee2
Marcotte173 <47604041+Marcotte173@users.noreply.github.com>
2019-04-17 15:24:44 -0600

    Add files via upload

--- Hockey Text Game/Hockey Text Game/Player.cs
+++ Hockey Text Game/Hockey Text Game/Player.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 public class Player
 {
-    string name;
-    int shot;
-    int pass;
-    int stick;
-    int run;
-    int def;
-    int save;
-    int school;
-
+    public string name;
+    public int shot;
+    public int pass;
+    public int stick;
+    public int run;
+    public int def;
+    public int save;
+    public int school;
+    public int canPlay;
+    public int chances;
+    
     public Player(string name, int shot, int pass, int stick, int run, int def, int save, int school)
     {
         this.name = name;
@@ -35,7 +37,11 @@ public class Player
     public int getDef() { return def; }
     public int getSave() { return save; }
     public int getSchool() { return school; }
+    public int getCanPlay() { return canPlay; }
+    public int getChances() { return chances; }
 
+    public void setChances(int i) { chances = i; }
+    public void setCanPlay(int i) { canPlay = i; }
     public void setShot(int i) { shot = i; }
     public void setPass(int i) { pass = i; }
     public void setStick(int i) { stick = i; }
--- Hockey Text Game/Hockey Text Game/Program.cs
+++ Hockey Text Game/Hockey Text Game/Program.cs
@@ -11,7 +11,7 @@ namespace Hockey_Text_Game
     {                                                                                    //shot, pass, stick, run, def, save, school
         public static List<Player> TeamSB = new List<Player>() { new Player("Beau", 8, 7, 8, 7, 7, 4, 7), new Player("Suhail", 5, 5, 5, 6, 7, 3, 8), new Player("Trook", 8, 7, 7, 6, 7, 3, 6), new Player("Parth", 7, 7, 6, 6, 6, 8, 6) };
         public static List<Player> TeamLinden = new List<Player>() { new Player("Linden", 8, 7, 8, 7, 7, 4, 7), new Player("Freeman", 5, 5, 5, 6, 7, 3, 8), new Player("Yashmeet", 8, 7, 7, 6, 7, 3, 6), new Player("Cohen", 7, 7, 6, 6, 6, 8, 6) };
-        public static List<Player> TeamAustin = new List<Player>() { new Player("Austin", 7, 7, 8, 7, 7, 4, 7), new Player("Haroon", 4, 4, 4, 6, 6, 6, 8), new Player("Pratsilopotchli", 4, 4, 4, 4, 5, 5, 8), new Player("Junaid", 7, 7, 6, 6, 6, 8, 6) };
+        public static List<Player> TeamAustin = new List<Player>() { new Player("Austin", 7, 7, 8, 7, 7, 4, 8), new Player("Haroon", 4, 4, 4, 6, 6, 6, 8), new Player("Pratsilopotchli", 4, 4, 4, 4, 5, 5, 8), new Player("Junaid", 7, 7, 6, 6, 6, 8, 6) };
         public static List<Player> TeamMarcus = new List<Player>() { new Player("Marcus", 6, 5, 5, 8, 6, 4, 8), new Player("Mankirat", 6, 6, 6, 6, 6, 4, 8), new Player("Paris", 6, 6, 6, 6, 6, 3, 8), new Player("Maclean", 4, 4, 4, 6, 5, 4, 8) };
         public static List<Player> TeamZaim = new List<Player>() { new Player("Zaim", 4, 4, 3, 7, 5, 6, 7), new Player("Lofstrand", 5, 5, 5, 6, 7, 4, 6), new Player("Anwesh", 4, 4, 3, 5, 5, 3, 7), new Player("Aditya", 3, 3, 3, 5, 5, 3, 7) };
         public static List<Player> TeamLewis = new List<Player>() {new Player("Lewis",8,7,7,7,7,4,7), new Player("Ayanah",5,5,5,6,7,3,8), new Player("Bell", 8,7,7,6,7,3,6), new Player("Tristan",7,7,7,7,7,8,6)  };
@@ -19,7 +19,17 @@ namespace Hockey_Text_Game
         public static List<string> TeamsPlaying = new List<string>() { };
         public static List<Player> TeamRoster0 = new List<Player>() { };
         public static List<Player> TeamRoster1 = new List<Player>() { };
-        
+        public static List<Player> gameRoster0 = new List<Player>() { };
+        public static List<Player> gameRoster1 = new List<Player>() { };
+        private static Random rand = new Random();
+        public static int period = 1;
+        public static int team0Def;
+        public static int team1Def;
+        public static int team0Chances;
+        public static int totalTeam0Chances;
+        public static int team1Chances;
+        public static int totalTeam1Chances;
+
 
         static void Main(string[] args)
         {
@@ -54,6 +64,8 @@ namespace Hockey_Text_Game
             {
                 
             } while (!int.TryParse(Console.ReadKey(true).KeyChar.ToString().ToLower(), out input));
+            if (input > TeamName.Count() - 1)
+                PickTeam();
             Console.WriteLine($"You have seleted {TeamName[input]}");
             RosterAdd(input, TeamsPlaying.Count);
             
@@ -84,7 +96,8 @@ namespace Hockey_Text_Game
         {
             Console.Clear();
             Console.WriteLine($"Get ready guys! {TeamsPlaying[0]} Vs {TeamsPlaying[1]} is about to begin!");
-            Console.ReadKey();
+            Keypress();
+            PlayerCheck();
         }
         
         private static void RosterAdd(int input, int count)
@@ -182,11 +195,114 @@ namespace Hockey_Text_Game
             
         }
 
+        private static void PlayerCheck()
+        {
+            Console.WriteLine($"\n\n{TeamsPlaying[0]} Roster check!");
+            for (int i = 0; i < TeamRoster0.Count; i++)
+            {
+                int roll = rand.Next(1, 11);
+                Console.Write($"\n{TeamRoster0[i].getName()}....");
+                bool canPlay = (roll <= TeamRoster0[i].getSchool()) ? true : false;
+                if (canPlay)
+                {
+                    Console.Write("can play!");
+                    gameRoster0.Add(TeamRoster0[i]);
+                }
+
+                else
+                {
+                    Console.Write("has to do homework!");
+                    
+                }
+            }
+            Console.WriteLine($"\n\n\n\n{TeamsPlaying[1]} Roster check!");
+            for (int i = 0; i < TeamRoster1.Count; i++)
+            {
+                int roll = rand.Next(1, 11);
+                Console.Write($"\n{TeamRoster1[i].getName()}....");
+                bool canPlay = (roll <= TeamRoster1[i].getSchool()) ? true : false;
+                if (canPlay)
+                {
+                    Console.Write("can play!");
+                    gameRoster1.Add(TeamRoster1[i]);
+                }
+
+                else
+                {
+                    Console.Write("has to do homework!");
+                    
+                }
+            }
+            Keypress();
+            PeriodStart();
+                                       
+        }
+
+
         private static void Keypress()
         {
             Console.WriteLine("\n\nPress any key to continue");
             Console.ReadKey(true);
         }
+
+        private static void PeriodStart()
+        {
+            Console.Clear();
+            Console.WriteLine($"It is Period {period}");
+            
+            for (int i = 0; i < (gameRoster0.Count); i++)
+            {
+                team0Def += gameRoster0[i].getDef();
+                team0Chances += gameRoster0[i].getStick() + gameRoster0[i].getRun();
+                
+            }
+            for (int i = 0; i < gameRoster1.Count; i++)
+            {
+                team1Def += gameRoster1[i].getStick();
+                team1Chances += gameRoster1[i].getStick() + gameRoster1[i].getRun();
+                
+
+            }
+
+            team0Def /= gameRoster0.Count;
+            team1Def /= gameRoster1.Count;
+            team0Chances -= team1Def;
+            team1Chances -= team0Def;
+            Console.WriteLine($"Team chances: {team0Chances} + {team1Chances}");
+
+            Console.WriteLine($"\n\n{TeamsPlaying[0]}\n");
+            for (int i = 0; i < gameRoster0.Count; i++)
+            {
+
+                int roll = rand.Next(-2, 3);
+                gameRoster0[i].setChances(((gameRoster0[i].getStick() + gameRoster0[i].getRun())/team0Chances)*100);
+                Console.WriteLine($"{gameRoster0[i].getName()} gets {gameRoster0[i].getChances()} chances to score");
+    
+            }
+            Console.WriteLine($"\n\n{TeamsPlaying[0]}\n");
+            for (int i = 0; i < gameRoster1.Count; i++)
+            {
+                int roll = rand.Next(-2, 3);
+                gameRoster1[i].setChances(((gameRoster1[i].getStick() + gameRoster1[i].getRun()) / team1Chances) * 100);
+
+                Console.WriteLine($"{gameRoster1[i].getName()} gets {gameRoster1[i].getChances()} chances to score");
+    
+            }
+            period++;
+
+            Keypress();
+            if (period > 3)
+                GameEvaluate();
+            else
+                PeriodStart();
+        }
+
+        private static void GameEvaluate()
+        {
+            Console.Clear();
+            Console.WriteLine("Next thing to do");
+        }
+
 
     }
 }
